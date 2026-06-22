2c45d7d27604be4d1a3f64c272930310582b9c6f
damlagulec <DAMLAEMINE.GUeLEC@ogr.gsu.edu.tr>
2026-06-23 00:24:19 +0300

    Update CustomUserAdmin list_display and fieldsets

--- users/admin.py
+++ users/admin.py
@@ -4,12 +4,12 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    # Listede hangi başlıklar görünsün?
+   
     list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
     
-    # Standart şifre ve kullanıcı ayarlarını kullan
+    
     fieldsets = UserAdmin.fieldsets 
     add_fieldsets = UserAdmin.add_fieldsets
 
-# Sadece bu satırı bırakıyoruz, unregister satırını tamamen sildik!
+
 admin.site.register(CustomUser, CustomUserAdmin)
