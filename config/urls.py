2d09b083c9a8393ee96fb359478f1f7a16a70169
damlagulec <DAMLAEMINE.GUeLEC@ogr.gsu.edu.tr>
2026-06-22 23:54:06 +0300

    Remove comments from urls.py for clarity

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Kullanıcı işlemleri (kayıt, giriş, çıkış, profil)
-    # users/urls.py dosyasına yönlendir
+    
     path('', include('users.urls')),
     
-    # İlan işlemleri (ana sayfa, ilan detay, yeni ilan vs.)
-    # ilanlar/urls.py dosyasına yönlendir
+    
     path('', include('ilanlar.urls')),
 ]
