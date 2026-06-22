0a3203f232cdd5a6839b940003b9b0c365d3330c
damlagulec <DAMLAEMINE.GUeLEC@ogr.gsu.edu.tr>
2026-06-22 23:56:11 +0300

    Refactor AdvertisementForm fields and widgets

--- ilanlar/forms.py
+++ ilanlar/forms.py
@@ -4,30 +4,29 @@ from .models import Advertisement
 class AdvertisementForm(forms.ModelForm):
     class Meta:
         model = Advertisement
-        # Harita koordinatları için latitude ve longitude alanlarını da forma dahil ettik
+        
         fields = ['title', 'category', 'description', 'location', 'latitude', 'longitude']
         
-        # TASARIM BURADA BAŞLIYOR: Her kutuya o 'form-control' stilini veriyoruz
+        
         widgets = {
             'title': forms.TextInput(attrs={
-                'class': 'form-control', # Mor ve köşeli tasarım
+                'class': 'form-control', 
                 'placeholder': 'Örn: Matematik Çalışma Grubu'
             }),
             'category': forms.Select(attrs={
-                'class': 'form-control', # Açılır liste için aynı stil
+                'class': 'form-control', 
             }),
             'description': forms.Textarea(attrs={
-                'class': 'form-control', # Büyük yazı alanı için aynı stil
+                'class': 'form-control', 
                 'rows': 4,
                 'placeholder': 'Hangi konularda çalışacağız? Saat kaçta?'
             }),
             'location': forms.TextInput(attrs={
                 'class': 'form-control', 
                 'placeholder': 'Örn: Haritadan seçin veya adres yazın...',
-                'id': 'map-search-input' # Google Haritalar API'si (Autocomplete) bu ID'yi kullanacak
+                'id': 'map-search-input' 
             }),
-            # Kullanıcı enlem ve boylamı elle girmeyecek, haritadan otomatik gelecek
-            # Bu yüzden bunları formda görünmez yapıyoruz.
+            
             'latitude': forms.HiddenInput(attrs={'id': 'id_latitude'}),
             'longitude': forms.HiddenInput(attrs={'id': 'id_longitude'}),
         }
